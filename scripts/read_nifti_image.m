ac166dcb8cba46c4e0567d0dd750e3b05acfe258
Stefan Pszczolkowski <stefanpsz@users.noreply.github.com>
2026-06-04 11:41:55 +0100

    Bugfix read_nifti_image.m

--- scripts/read_nifti_image.m
+++ scripts/read_nifti_image.m
@@ -1,5 +1,5 @@
 function nii = read_nifti_image(path)
-    nii = read_nifti_image(path);
+    nii = load_untouch_nii(path);
     nii.img(isinf(nii.img)) = 0;
     nii.img(isnan(nii.img)) = 0;
     nii.img(nii.img < 0) = 0;
